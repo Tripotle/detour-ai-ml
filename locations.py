0e033abb0640ad68bbcde5b94d7e576c3d757f19
tropylium <adg4096@gmail.com>
2023-03-08 01:23:48 -0500

    Add places api test to test_api function

--- locations.py
+++ locations.py
@@ -9,6 +9,17 @@ TEST_ADDRESS = '48 Massachusetts Ave w16, Cambridge, MA 02139'
 def test_api(address=TEST_ADDRESS):
     gmaps = googlemaps.Client(key=api.get_api_key())
 
+    try:
+        place = gmaps.find_place(
+            "park",
+            "textquery",
+            fields=["geometry/location", "place_id"],
+            location_bias="point:90,90",
+        )
+        pprint(place)
+    except googlemaps.exceptions.ApiError as e:
+        pprint(e)
+
     # https://developers.google.com/maps/documentation/javascript/geocoding#GeocodingRequests
     try:
         result = gmaps.geocode(address)
