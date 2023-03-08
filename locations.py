960e0ac1b131f2cd2b1d2b2359234ed216c75c84
tropylium <adg4096@gmail.com>
2023-03-08 01:09:51 -0500

    add basic api skeleton, google maps api test function

--- locations.py
+++ locations.py
@@ -1,5 +1,22 @@
 import api
+import googlemaps
+from pprint import pprint
+
+# Kresge
+TEST_ADDRESS = '48 Massachusetts Ave w16, Cambridge, MA 02139'
+
+
+def test_api(address=TEST_ADDRESS):
+    gmaps = googlemaps.Client(key=api.get_api_key())
+
+    # https://developers.google.com/maps/documentation/javascript/geocoding#GeocodingRequests
+    try:
+        result = gmaps.geocode(address)
+        pprint(result)
+    except googlemaps.exceptions.ApiError as e:
+        pprint(e)
 
 
 if __name__ == '__main__':
     print(f'api key is {api.get_api_key()}')
+    test_api()
