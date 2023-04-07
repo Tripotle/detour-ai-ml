f80f71b8f7bb9d9484edb62bac574890669a62ff
ElSnako314 <fle@mit.edu>
2023-04-06 22:06:55 -0400

    Update directions.py

--- directions.py
+++ directions.py
@@ -21,6 +21,7 @@ def get_polyline(origin=TEST_ORIGIN, destination=TEST_DESTINATION):
         overview_polyline = query_result[0]['overview_polyline']['points']
         distance = query_result[0]['legs'][0]['distance']['value']
         return overview_polyline, distance
+    
     except googlemaps.exceptions.ApiError as e:
         print(origin, destination)
         print("API Error!")
@@ -34,19 +35,29 @@ def possible_detours(waypoints, distance, increment=20):
     detours = set()
     detour_positions = set()
     for waypoint in waypoints[::increment]:
-        for location in get_nearby_places(page_token="",location=waypoint,radius=distance/increment):
+        for location in get_nearby_places(page_token="",location=waypoint,radius=20000):
             if location.position not in detour_positions:
                 detour_positions.add(location.position)
                 detours.add(location)
-    
     return detours
+
+def get_reviews(detours):
+    gmaps = googlemaps.Client(key=api.get_api_key())
+
+    for detour_id in detours:
+        content = gmaps.place(
+            place_id='ChIJ-SCVle9544kRt47b6znsdQM'
+        )
+        pprint(content)
 
 if __name__ == '__main__':
     # print(sys.version)
     overview_polyline, distance = get_polyline()
     waypoints = get_waypoints(overview_polyline)
+    # print(len(waypoints), distance)
     detours = possible_detours(waypoints, distance, increment=50)
     for place_index, place in enumerate(detours):
         print(place_index)
         pprint(str(place), width=1000)
         pprint(place.get_gmaps_link())
+    # get_reviews([1])
