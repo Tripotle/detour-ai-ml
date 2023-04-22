2588c88b0e110ae9cbc0fb3b07b198f0aac79ecf
tropylium <adg4096@gmail.com>
2023-04-22 18:29:13 -0400

    add data collection test

--- src/data_collection/__init__.py
+++ src/data_collection/__init__.py
@@ -1,4 +1,3 @@
-import urllib.parse
 from typing import List
 from data_collection.types import Location, Position
 from data_collection.directions import get_detours
@@ -13,3 +12,13 @@ def get_locations(origin: str, destination: str) -> List[Location]:
     :return: see above
     """
     return get_detours(origin, destination)
+
+
+if __name__ == '__main__':
+    # Strictly for testing only
+    from pprint import pprint
+    TEST_ORIGIN = 'ChIJh2oa9apw44kRPCAIs6WO4NA' # MIT
+    TEST_DESTINATION = 'ChIJLw8wo4Vw44kRWkWR0c03LH4' # Boston City Hall
+    result = get_locations(TEST_ORIGIN, TEST_DESTINATION)
+    pprint(f"total locations: {len(result)}")
+    pprint(result)
