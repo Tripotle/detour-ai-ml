86e66d55da16aff1106eac3f9e9116e88a672537
tropylium <adg4096@gmail.com>
2023-04-22 18:10:02 -0400

    add data collection package

--- src/data_collection/places.py
+++ src/data_collection/places.py
@@ -1,6 +1,7 @@
 import api
 import googlemaps
 from pprint import pprint
+from data_collection import Location, Position
 import urllib.parse
 
 # Kresge
@@ -118,28 +119,6 @@ EXCLUDE_PLACE_TYPES: list[str] = [
     "street_number",
     "sublocality",
 ]
-
-Position = tuple[float, float]  # [lat, long]
-
-
-class Location:
-
-    def __init__(self, name: str, position: Position, place_id: str, types: list[str], rating: int, num_ratings: int):
-        self.position = position
-        self.name = name
-        self.place_id = place_id
-        self.types = types
-        self.rating = rating
-        self.num_ratings = num_ratings
-        self.information = []
-
-    def get_gmaps_link(self) -> str:
-        return f'https://www.google.com/maps/search/?api=1' \
-               f'&query={urllib.parse.quote(self.name)}' \
-               f'&query_place_id={urllib.parse.quote(self.place_id)}'
-
-    def __str__(self):
-        return f'{self.name}, ID: {self.place_id}, types: {", ".join(self.types)}'
 
 
 def from_query_result(query_result: any) -> Location:
